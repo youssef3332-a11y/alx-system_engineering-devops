cb80345322c57d1a513302097bbbf7cb58da2de7
youssef3332-a11y <youssef17102003@gmail.com>
2024-08-16 00:08:06 +0100

    new file

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,27 +1,19 @@
 #!/usr/bin/python3
-
-"""[task 0, get rest api]
 """
-import requests
+queries the Reddit API and returns the number of subscribers (not active users,
+total subscribers) for a given subreddit. If an invalid subreddit is given,
+return 0
+"""
+from requests import get
+from sys import argv
 
 
 def number_of_subscribers(subreddit):
-    """returns the number of subscribers
-
-    Args:
-        subreddit ([string]): [type of subreddit to consult]
-
-    Returns:
-        [int]: [# of subscribers]
-    """
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    about_info = requests.get(url, headers={"User-Agent": "dairof7"},
-                              allow_redirects=False)
-    if about_info.status_code == 200:
-        return about_info.json()['data']['subscribers']
-    else:
+    """returns number of subscribers for a given subreddit"""
+    r_subreddit = get('http://www.reddit.com/r/{}/about.json'.
+                      format(subreddit), headers={'User-Agent': 'Mybot'})
+    try:
+        subreddit_dict = r_subreddit.json()
+        return subreddit_dict['data']['subscribers']
+    except:
         return 0
-
-
-if __name__ == "__main__":
-    pass
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,27 +1,18 @@
 #!/usr/bin/python3
-
-"""[task 0, get rest api]
 """
-import requests
+queries the Reddit API and prints the titles of the first
+10 hot posts listed for a given subreddit
+"""
+from requests import get
 
 
 def top_ten(subreddit):
-    """print the top 10 hot post
-
-    Args:
-        subreddit ([string]): [type of subreddit to consult]
-
-    """
-    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
-    about_info = requests.get(url, headers={"User-Agent": "dairof7"},
-                              allow_redirects=False)
-
-    if about_info.status_code == 200:
-        for post in about_info.json()['data']['children']:
-            print(post['data']['title'])
-    else:
+    """prints titles of first 10 hot posts listed for a given subreddit"""
+    r_subreddit = get('http://www.reddit.com/r/{}/hot.json'.
+                      format(subreddit), headers={'User-Agent': 'Mybot'})
+    try:
+        subreddit_dict = r_subreddit.json()
+        for i in range(10):
+            print(subreddit_dict['data']['children'][i]['data']['title'])
+    except:
         print(None)
-
-
-if __name__ == "__main__":
-    pass
