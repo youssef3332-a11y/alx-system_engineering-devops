4623a64900c665605e5840740c9e2052bc64a890
aitgouraine.youssef <78111236+youssef3332-a11y@users.noreply.github.com>
2024-09-01 18:01:01 +0100

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,7 +9,7 @@ import requests
 def number_of_subscribers(subreddit):
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {'User-Agent': 'my-app/0.0.1'}
-
+    
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
         if response.status_code == 200:
